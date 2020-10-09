b69472f5d229dc0f5c2eed5f1cfd744660c4a165
Deepak Nigam <deepak.nigam@noitechnologies.com>
2020-10-09 12:58:25 +0530

    [SLDT-367] Changes corresponding to use of ES7 in vue storefront.

---
+++ LoadData.sh
@@ -0,0 +1,31 @@
+# This command requires "jq" -> https://stedolan.github.io/jq/
+if ! [ -x "$(command -v jq)" ]; then
+  echo 'Error: jq is not installed. Please download it from https://stedolan.github.io/jq/' >&2
+  exit 1
+fi
+
+echo "Starting import........."
+
+cd $1
+
+cd var
+
+apiUrl="${2}products?productStoreId=${3}"
+curl -X GET $apiUrl -H 'authorization: Basic YXBpLnVzZXI6Iyojc2FsZGlzdEAxMjM='   -H 'cache-control: no-cache'   -H 'content-type: application/json' | jq ".data[]" | jq  -M \ > catalog_product.json
+sed -i 's/"null"/null/g' catalog_product.json
+
+apiUrl="${2}attributes?productStoreId=${3}"
+curl -X GET $apiUrl -H 'authorization: Basic YXBpLnVzZXI6Iyojc2FsZGlzdEAxMjM='   -H 'cache-control: no-cache'   -H 'content-type: application/json' | jq ".data[]" | jq  -M \ > catalog_attribute.json
+sed -i 's/"null"/null/g' catalog_attribute.json
+
+apiUrl="${2}categories?productStoreId=${3}"
+curl -X GET $apiUrl -H 'authorization: Basic YXBpLnVzZXI6Iyojc2FsZGlzdEAxMjM='   -H 'cache-control: no-cache'   -H 'content-type: application/json' | jq ".data[]" | jq  -M \ > catalog_category.json
+sed -i 's/"null"/null/g' catalog_category.json
+
+echo "Dumping data into Elastic Server"
+
+cd ..
+
+yarn restore
+
+echo "Imported Successfully........."
